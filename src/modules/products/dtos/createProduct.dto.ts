933bcc50c615b51f051d42858adb27a6b236e274
AndreaQuelali <andrealizbethquelali5@gmail.com>
2025-11-01 23:57:07 -0400

    feat: add interface of create and update product

---
+++ src/modules/products/dtos/createProduct.dto.ts
@@ -0,0 +1,9 @@
+export interface ICreateProduct {
+    name: string;
+    price: number;
+    stock: number;
+    unit: string;
+    category: string;
+    description: string;
+    image: string;  
+}
---
+++ src/modules/products/dtos/updateProduct.ts
@@ -0,0 +1,8 @@
+export interface IUpdateProduct {
+    price: number;
+    stock: number;
+    unit: string;
+    category: string;
+    description: string;
+    image: string;
+}
