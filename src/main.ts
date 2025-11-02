e1a714efe01529a7f4f62798071a09ebaf7f1703
AndreaQuelali <andrealizbethquelali5@gmail.com>
2025-11-01 23:55:06 -0400

    feat: update main.ts with route

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,13 @@
 import express from "express";
 import bodyParser from "body-parser";
 import dotenv from "dotenv";
-import healthRouter from "./modules/healthCheck/healthCheck.routes";
-import productRouter from "./modules/products/products.routes";
+import router from "./config/server.routes";
 
 dotenv.config();
 const PORT = process.env.PORT || 3001;
 const app = express();
 app.use(bodyParser.json());
-app.use("/", healthRouter);
-app.use("/", productRouter);
+app.use("/", router);
 
 
 app.listen(PORT, () => {
