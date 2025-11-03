96e80749bfab18b318ffe2f8e221362a4213e1af
AndreaQuelali <andrealizbethquelali5@gmail.com>
2025-11-03 19:42:37 -0400

    feat: update main con ENV and prisma connect

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,27 @@
 import express from "express";
 import bodyParser from "body-parser";
-import dotenv from "dotenv";
 import router from "./config/server.routes";
+import { prisma } from "./config/prisma";
+import { ENV } from "./config/env.config";
 
-dotenv.config();
-const PORT = process.env.PORT || 3001;
-const app = express();
-app.use(bodyParser.json());
-app.use("/", router);
+async function init() {
+    try {
+        await prisma.$connect();
+        console.log("Database connection established");
+
+        const PORT = ENV.PORT || 3001;
+        const app = express();
+        app.use(bodyParser.json());
+        app.use("/", router);
 
 
-app.listen(PORT, () => {
-    console.log(`Server running on port http://localhost:${PORT}`);
-});
+        app.listen(PORT, () => {
+            console.log(`Server running on port http://localhost:${PORT}`);
+        });
+    } catch (error) {
+        console.error("Error connecting to the database:", error);
+        process.exit(1);
+    }
+}
+
+init();
