9cda77300fcfef301c6d8bf97c6a9bec06fb81a5
AndreaQuelali <andrealizbethquelali5@gmail.com>
2025-11-07 18:35:39 -0400

    feat: add JWT, PWD, SALTS, REDIS_URL

--- src/config/env.config.ts
+++ src/config/env.config.ts
@@ -9,4 +9,9 @@ export const ENV = {
     PGDATA: process.env.PGDATA || "/var/lib/postgresql/data/pgdata",
     PGHOST: process.env.PGHOST || "localhost",
     PGPORT: process.env.PGPORT || 5433,
+    NODE_ENV: process.env.NODE_ENV || "development",
+    JWT_SECRET: process.env.JWT_SECRET || "secretKey",
+    PWD_SECRET: process.env.PWD_SECRET || "secretPwd",
+    SALTS: process.env.SALTS || 512,
+    REDIS_URL: process.env.REDIS_URL || "redis://localhost:6379",
 }
