3607380120b7597631f467208cd7a7be68659e20
AndreaQuelali <andrealizbethquelali5@gmail.com>
2025-11-07 18:35:13 -0400

    feat: add inirSOcket and initMailWorker

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,9 @@ import bodyParser from "body-parser";
 import router from "./config/server.routes";
 import { prisma } from "./config/prisma";
 import { ENV } from "./config/env.config";
+import http from "http";
+import { initSocket } from "./config/socket";
+import { initMailWorker } from "./tools/mail.tool";
 
 async function init() {
     try {
@@ -14,8 +17,13 @@ async function init() {
         app.use(bodyParser.json());
         app.use("/", router);
 
+        const server = http.createServer(app);
+        initSocket(server);
+        console.log("Socket connection established");
+        initMailWorker();
+        console.log("Mail worker initialized");
 
-        app.listen(PORT, () => {
+        server.listen(PORT, () => {
             console.log(`Server running on port http://localhost:${PORT}`);
         });
     } catch (error) {
